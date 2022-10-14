b2039d6cb6e097cb9ca654ab62456f8332cc7498
UzoAfric <ibezimuzomajoseph@gmail.c>
2022-10-14 21:23:14 +0100

    Final Priority Queue with test

--- untitled/src/running.java
+++ untitled/src/running.java
@@ -1,11 +1,89 @@
 import org.jetbrains.annotations.NotNull;
+    class Counter
+    {
+        int count;
+        public synchronized void increment()
+        {
+        count++;
+        }
+    }
+    public class running {
+        public static void main(String[] args) throws Exception {
+            Counter c = new Counter();
 
-public class running {
-    
+            Thread t1 = new Thread(new Runnable()
+            {
+                public void run()
+                {
+            for(int i=1; i<=1000; i++)
+            {
+                c.increment();
+            }
+        }
+            });
+            Thread t2 = new Thread(new Runnable()
+            {
+                public void run()
+                {
+            for(int i=1; i<=1000; i++)
+            {
+                c.increment();
+            }
+        }
+            });
 
-}
+            t1.start();
+            t2.start();
+
+            t1.join();
+            t2.join();
+            System.out.println("Count " + c.count);
+        }
+
+    }
+//        public static void main(String[] args) throws Exception {
+//
+//            Thread t1 = new Thread(() ->
+//            {
+//                for (int i = 1; i <5 ; i++) {
+//                    System.out.println("Hi");
+//                    try{ Thread.sleep(1000); } catch(Exception e ){}
+//                }
+//            });
+//            Thread t2 = new Thread(()  ->
+//            {
+//                for (int i = 1; i < 5; i++) {
+//                    System.out.println("Hello");
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (Exception e) {
+//                    }
+//                }
+//            });
+//
+//            t1.setPriority(1);
+//            t2.setPriority(10);
+//            System.out.println(t1.getPriority());
+//            System.out.println(t2.getPriority());
+//
+//
+//            t1.start();
+//            try {
+//                Thread.sleep(1000);
+//            } catch (Exception e) {
+//            }
+//            t2.start();
+//
+//            t1.join();
+//            t2.join();
+//
+//            System.out.println("BYE");
+//        }
+//
+//    }
 
 
+  
 
 
 
